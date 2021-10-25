7d4381cf2bf3f36f373d344967ce6d23e2200eb4
Braedon Watkins <watkins.braedon@gmail.com>
2021-10-25 15:11:21 -0400

    Git Test

--- frontend/src/components/CardUI.js
+++ frontend/src/components/CardUI.js
@@ -5,6 +5,7 @@ function CardUI()
     var card = '';
     var search = '';
 
+    //git test
     // eslint-disable-next-line
     const [message,setMessage] = useState('');
     // eslint-disable-next-line
