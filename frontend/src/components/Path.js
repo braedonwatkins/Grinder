bac0985d8026ded0324bc708ef7b3b38821616d5
Jerry4753 <74114017+Jerry4753@users.noreply.github.com>
2021-10-29 23:21:11 -0400

    Fixed app_name

--- frontend/src/components/Path.js
+++ frontend/src/components/Path.js
@@ -1,14 +1,9 @@
-const app_name = 'cop4331-57'
+const app_name = "cop4331-grinder";
 
-exports.buildPath = 
-function buildPath(route)
-{
-    if (process.env.NODE_ENV === 'production') 
-    {
-        return 'https://' + app_name +  '.herokuapp.com/' + route;
-    }
-    else
-    {        
-        return 'http://localhost:5000/' + route;
-    }
-}
+exports.buildPath = function buildPath(route) {
+  if (process.env.NODE_ENV === "production") {
+    return "https://" + app_name + ".herokuapp.com/" + route;
+  } else {
+    return "http://localhost:5000/" + route;
+  }
+};
